79b4817d2c4d665f89441ead7ca95356fc5507d1
REOScotte <11577865+REOScotte@users.noreply.github.com>
2024-11-17 03:58:20 -0600

    more

--- InstallLab.ps1
+++ InstallLab.ps1
@@ -4,9 +4,13 @@ $config.Keys | ForEach-Object {
     New-Variable -Name $_ -Value $config.$_ -Force
 }
 
+$PublicNetwork = [ipaddress]($PublicRouterIP.Address -band $PublicRouterSubnetMask.Address)
+
 # Create Public VM Switch
-$existingPublicSwitch = Get-VMSwitch | Where-Object Name -EQ $config.PublicSwitchName
-if (-not $existingPublicSwitch) {
-    $existingPublicSwitch = New-VMSwitch -Name $config.PublicVMSwitchName -SwitchType External -NetAdapterName $config.PublicAdapterName -AllowManagementOS $false
+$allVMSwitches = Get-VMSwitch
+$existingPublicVMSwitch = $allVMSwitches | Where-Object Name -EQ $config.PublicVMSwitchName
+if (-not $existingPublicVMSwitch) {
+    if ($allVMSwitches) {}
+    $existingPublicVMSwitch = New-VMSwitch -Name $config.PublicVMSwitchName -NetAdapterName $config.PhysicalAdapterName -AllowManagementOS $true
 }
 
--- LabConfig.ps1
+++ LabConfig.ps1
@@ -1,9 +1,13 @@
 @{
     ServerISOPath          = 'C:\Install\26100.1742.240906-0331.ge_release_svc_refresh_SERVER_EVAL_x64FRE_en-us.iso'
     PrivateIPNetwork       = '10.0.0.0/24'
+
+    PublicVMSwitchName     = 'Externals'
+    PrivateVMSwitchName    = 'PoshLab'
+
+    PhysicalAdapterName    = 'Wi-Fi'
     PublicRouterIP         = [ipaddress]'192.168.0.200'
     PublicRouterSubnetMask = [ipaddress]'255.255.255.0'
-    PublicVMSwitchName     = 'External'
-    PrivateVMSwitchName    = 'PoshLab'
-    PhysicalAdapterName    = 'Ethernet'
+
+    ExternalDNSServer      = '8.8.8.8'
 }
